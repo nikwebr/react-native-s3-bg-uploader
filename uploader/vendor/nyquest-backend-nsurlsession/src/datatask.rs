c920e1b662609a2c7e9edadb08e339c8d069d416
Niklas Weber <41891479+nikwebr@users.noreply.github.com>
2026-05-01 16:59:43 +0200

    fix for ios

---
+++ uploader/vendor/nyquest-backend-nsurlsession/src/datatask.rs
@@ -0,0 +1,7 @@
+mod delegate;
+mod generic_waker;
+mod ivars;
+mod rcblock_ext;
+
+pub(crate) use delegate::{DataTaskDelegate, DataTaskSharedContextRetained};
+pub(crate) use generic_waker::GenericWaker;
